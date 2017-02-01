fc03577209ed8007324049ffff64cc25791ef3b2
webpwnized <webpwnized@gmail.com>
2017-01-31 19:57:29 -0500

    Updated shifty and freek
    Added involutary key detection and better argument checking into Shifty.
    Added frequency comparison into freek.

--- freek.py
+++ freek.py
@@ -1,7 +1,7 @@
 from collections import Counter
 import argparse
 
-gEnglishLetterFrequencies = {'E': 12.70, 'T': 9.06, 'A': 8.17, 'O': 7.51, 'I': 6.97, 'N': 6.75, 'S': 6.33, 'H': 6.09, 'R': 5.99,
+ENGLISH_LETTERS_FREQUENCY = {'E': 12.70, 'T': 9.06, 'A': 8.17, 'O': 7.51, 'I': 6.97, 'N': 6.75, 'S': 6.33, 'H': 6.09, 'R': 5.99,
                      'D': 4.25, 'L': 4.03, 'C': 2.78, 'U': 2.76, 'M': 2.41, 'W': 2.36, 'F': 2.23, 'G': 2.02, 'Y': 1.97,
                      'P': 1.93, 'B': 1.29, 'V': 0.98, 'K': 0.77, 'J': 0.15, 'X': 0.15, 'Q': 0.10, 'Z': 0.07}
 
@@ -11,30 +11,66 @@ LETTERS_DICTIONARY = {'A': 0, 'B': 0, 'C': 0, 'D': 0, 'E': 0, 'F': 0, 'G': 0, 'H
                 'M': 0, 'N': 0, 'O': 0, 'P': 0, 'Q': 0, 'R': 0, 'S': 0, 'T': 0, 'U': 0, 'V': 0, 'W': 0, 'X': 0,
                 'Y': 0, 'Z': 0}
 
-def do_analysis(pText):
+
+def do_print_frequency(pDictionary, pTotalCount):
+
+    TWO_DECIMAL_PLACES = "{0:.2f}"
+    lFrequencyPercent = 0
+
+    print()
+    for letter, frequency in pDictionary.items():
+        lFrequencyPercent = frequency / pTotalCount * 100
+        lBestMatch = ''
+        lBestFrequencyMatch = 100.0
+        lCurrentDifference = 100.00
+        lSmallestDifference = 100.00
+        for englishLetter, englishLetterFrequency in ENGLISH_LETTERS_FREQUENCY.items():
+            lCurrentDifference = abs(lFrequencyPercent - englishLetterFrequency)
+            if lCurrentDifference < lSmallestDifference:
+                lBestMatch = englishLetter
+                lBestFrequencyMatch = englishLetterFrequency
+                lSmallestDifference = lCurrentDifference
+            #end if
+        #end for
+        lFrequencyPercentString = TWO_DECIMAL_PLACES.format(lFrequencyPercent)
+        lBestFrequencyMatchString = 'Best Match: ' + lBestMatch + '(' + TWO_DECIMAL_PLACES.format(lBestFrequencyMatch) + ')'
+        print(letter, frequency, lFrequencyPercentString, lBestFrequencyMatchString)
+
+
+def do_print_histogram(pDictionary, pTotalCount):
+
+    lFrequencyBarLength = 0
+
+    print()
+    for letter, frequency in pDictionary.items():
+        lFrequencyBarLength = int(frequency / pTotalCount * 500)
+        print(letter, frequency, "#" * lFrequencyBarLength)
+
+def do_analysis(pTextLine):
 
     lTotalCount = 0
 
-    for character in pText.upper():
+    for character in pTextLine.upper():
         if character in LETTERS_SORTED_ALPHABETICALLY:
             LETTERS_DICTIONARY[character]+=1
             lTotalCount+=1
 
-    for letter, frequency in LETTERS_DICTIONARY.items():
-        print(letter,frequency,"{0:.2f}".format(frequency/lTotalCount*100),"#"*int(frequency/lTotalCount*500))
+    do_print_frequency(LETTERS_DICTIONARY, lTotalCount)
+    do_print_histogram(LETTERS_DICTIONARY, lTotalCount)
 
-def do_analysis(pFile):
+
+def do_analysis(pTextFile):
 
     lTotalCount = 0
 
-    for line in pFile:
+    for line in pTextFile:
         for character in line.upper():
             if character in LETTERS_SORTED_ALPHABETICALLY:
                 LETTERS_DICTIONARY[character]+=1
                 lTotalCount+=1
 
-    for letter, frequency in LETTERS_DICTIONARY.items():
-        print(letter,frequency,"{0:.2f}".format(frequency/lTotalCount*100),"#"*int(frequency/lTotalCount*500))
+    do_print_frequency(LETTERS_DICTIONARY, lTotalCount)
+    do_print_histogram(LETTERS_DICTIONARY, lTotalCount)
 
 
 if __name__ == '__main__':
--- shifty.py
+++ shifty.py
@@ -11,12 +11,12 @@ def do_encryption_shift_cipher(pPlaintext, pKey):
     #Encrypt the string and return the ciphertext
     result = ''
 
-    for l in pPlaintext.lower():
+    for lLetter in pPlaintext.lower():
         try:
-            i = (ALPHABET.index(l) + pKey) % 26
+            i = (ALPHABET.index(lLetter) + pKey) % 26
             result += ALPHABET[i]
         except ValueError:
-            result += l
+            result += lLetter
 
     return result.lower()
 
@@ -25,20 +25,26 @@ def do_decryption_shift_cipher(pCiphertext, pKey):
     #Decrypt the string and return the plaintext
     result = ''
 
-    for l in pCiphertext:
+    for lLetter in pCiphertext:
         try:
-            i = (ALPHABET.index(l) - pKey) % 26
+            i = (ALPHABET.index(lLetter) - pKey) % 26
             result += ALPHABET[i]
         except ValueError:
-            result += l
+            result += lLetter
 
     return result
 
+
+def key_is_involutary(pKey):
+    return 2 * pKey % 26 == 0
 
 def show_result(pText, pKey, pEncrypt):
 
     print('Text: {}'.format(pText))
     print('Key: {}'.format(pKey))
+
+    if key_is_involutary(pKey):
+        print('[*] Warning: Key {} is involutary'.format(pKey))
 
     pText = pText.lower()
 
@@ -68,12 +74,16 @@ def encrypt(pText, pKey):
 if __name__ == '__main__':
 
     lArgParser = argparse.ArgumentParser()
-    lArgParser.add_argument('-e', '--encrypt', help='Encrypt TEXT. This option requires a KEY.', action='store_true')
-    lArgParser.add_argument('-d', '--decrypt', help='Decrypt TEXT. This option requires a KEY or BRUTEFORCE flag.', action='store_true')
-    lArgParser.add_argument('-b', '--bruteforce', help='Rather than decrypt with KEY, try to brute force the plaintext.', action='store_true')
+    lEncryptionActionGroup = lArgParser.add_mutually_exclusive_group(required=True)
+    lEncryptionActionGroup.add_argument('-e', '--encrypt', help='Encrypt TEXT. This option requires a KEY.', action='store_true')
+    lEncryptionActionGroup.add_argument('-d', '--decrypt', help='Decrypt TEXT. This option requires a KEY or BRUTEFORCE flag.', action='store_true')
+    lEncryptionActionGroup.add_argument('-b', '--bruteforce', help='Rather than decrypt with KEY, try to brute force the plaintext.', action='store_true')
     lArgParser.add_argument('-k', '--key', help='Encryption/Decription key', type=int, action='store')
     lArgParser.add_argument('TEXT', help='Text value to encrypt/decrypt', type=str, action='store')
     lArgs = lArgParser.parse_args()
+
+    if (lArgs.encrypt or lArgs.decrypt) and not lArgs.key:
+        lArgParser.error('If -e/--encrypt or -d/--decrypt selected, -k/--key is required')
 
     if lArgs.encrypt:
         encrypt(lArgs.TEXT, lArgs.key)

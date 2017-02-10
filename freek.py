4ed715f8113267ffc571afd8b0864a93cee26e1d
webpwnized <webpwnized@gmail.com>
2017-02-09 20:17:13 -0500

    Added IOC to freek

--- freek.py
+++ freek.py
@@ -1,48 +1,54 @@
-from collections import Counter
 import argparse, base64
 
-# ENGLISH_LETTERS_FREQUENCY = {'E': 12.70, 'T': 9.06, 'A': 8.17, 'O': 7.51, 'I': 6.97, 'N': 6.75, 'S': 6.33, 'H': 6.09, 'R': 5.99,
-#                      'D': 4.25, 'L': 4.03, 'C': 2.78, 'U': 2.76, 'M': 2.41, 'W': 2.36, 'F': 2.23, 'G': 2.02, 'Y': 1.97,
-#                      'P': 1.93, 'B': 1.29, 'V': 0.98, 'K': 0.77, 'J': 0.15, 'X': 0.15, 'Q': 0.10, 'Z': 0.07}
-#
-# LETTERS_SORTED_BY_POPULARITY = 'ETAOINSHRDLCUMWFGYPBVKJXQZ'
-# LETTERS_SORTED_ALPHABETICALLY = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-# LETTERS_DICTIONARY = {'A': 0, 'B': 0, 'C': 0, 'D': 0, 'E': 0, 'F': 0, 'G': 0, 'H': 0, 'I': 0, 'J': 0, 'K': 0, 'L': 0,
-#                 'M': 0, 'N': 0, 'O': 0, 'P': 0, 'Q': 0, 'R': 0, 'S': 0, 'T': 0, 'U': 0, 'V': 0, 'W': 0, 'X': 0,
-#                 'Y': 0, 'Z': 0}
-#
-#
-# def do_print_frequency(pDictionary, pTotalCount):
-#
-#     TWO_DECIMAL_PLACES = "{0:.2f}"
-#     lFrequencyPercent = 0
-#
-#     print()
-#     for letter, frequency in pDictionary.items():
-#         lFrequencyPercent = frequency / pTotalCount * 100
-#         lBestMatch = ''
-#         lBestFrequencyMatch = 100.0
-#         lCurrentDifference = 100.00
-#         lSmallestDifference = 100.00
-#         for englishLetter, englishLetterFrequency in ENGLISH_LETTERS_FREQUENCY.items():
-#             lCurrentDifference = abs(lFrequencyPercent - englishLetterFrequency)
-#             if lCurrentDifference < lSmallestDifference:
-#                 lBestMatch = englishLetter
-#                 lBestFrequencyMatch = englishLetterFrequency
-#                 lSmallestDifference = lCurrentDifference
-#             #end if
-#         #end for
-#         lFrequencyPercentString = TWO_DECIMAL_PLACES.format(lFrequencyPercent)
-#         lBestFrequencyMatchString = 'Best Match: ' + lBestMatch + '(' + TWO_DECIMAL_PLACES.format(lBestFrequencyMatch) + ')'
-#         print(letter, frequency, lFrequencyPercentString, lBestFrequencyMatchString)
+
+TWO_DECIMAL_PLACES = "{0:.2f}"
 
 
-def do_print_histogram(pByteCounts: dict, pTotalBytes: int, pShowHistogram: bool, pShowASCII: bool, pPercent: bool, pVerbose: bool) -> None:
+def get_delta_index_of_coincidence(pInput: bytearray) -> dict:
+    # IOC:
+    # Calculate the frequency of each byte and total number of bytes
+    # For each frequency f, calculate f(f - 1) and sum these values
+    MAX_POSITIONS_TO_ANALYZE = 300
+
+    lBytesOfInput = len(pInput)
+    lPositionsToAnalyze = min(lBytesOfInput, MAX_POSITIONS_TO_ANALYZE)
+    lIOC = dict.fromkeys(range(1, lPositionsToAnalyze), 0)
+    for lBytesShifted in range(1, lPositionsToAnalyze):
+        lMatches = 0
+        lBytesToTest = lPositionsToAnalyze - lBytesShifted
+        for lByte in range(0, lBytesToTest):
+            if pInput[lByte] == pInput[lByte + lBytesShifted]:
+                lMatches += 1
+            # end if
+        # end for lByte
+        lIOC[lBytesShifted] = lMatches / lBytesToTest
+    # end for lBytesShifted
+
+    return lIOC
+
+
+def print_delta_index_of_coincidence(pInput: bytearray) -> None:
+    lIOC = get_delta_index_of_coincidence(pInput)
+
+    lScaleFactor = 10
+
+    for lByteOffset, lCoincidence in lIOC.items():
+        lPercentCoincidence = lCoincidence * 100
+        lCoincidenceBarLength = int(lPercentCoincidence * lScaleFactor)
+
+        lOutput = 'Byte Offset: '
+        lOutput += str(lByteOffset) + ' '
+        lOutput += "(" + TWO_DECIMAL_PLACES.format(lPercentCoincidence) + "%) "
+        lOutput += "#" * lCoincidenceBarLength
+        print(lOutput)
+    # end for
+
+
+def print_results(pByteCounts: dict, pTotalBytes: int, pShowHistogram: bool, pShowASCII: bool, pPercent: bool, pVerbose: bool) -> None:
 
     lScaleFactor = 20
 
     for lByte, lByteCount in pByteCounts.items():
-        TWO_DECIMAL_PLACES = "{0:.2f}"
         lPercent = lByteCount / pTotalBytes * 100
         lFrequencyBarLength = int(lPercent * lScaleFactor)
 
@@ -59,8 +65,8 @@ def do_print_histogram(pByteCounts: dict, pTotalBytes: int, pShowHistogram: bool
             if pShowHistogram:
                 lOutput += "#" * lFrequencyBarLength
             print(lOutput)
-        #end if
-    #end if
+        # end if
+    # end for
 
 
 def analyze(pInput: bytearray, pShowHistogram: bool, pShowASCII: bool, pPercent: bool, pVerbose: bool) -> None:
@@ -68,10 +74,11 @@ def analyze(pInput: bytearray, pShowHistogram: bool, pShowASCII: bool, pPercent:
     lTotalBytes = 0
 
     for lByte in pInput:
-        lByteCounts[lByte] +=1
+        lByteCounts[lByte] += 1
         lTotalBytes += 1
+    # end for
 
-    do_print_histogram(lByteCounts, lTotalBytes, pShowHistogram, pShowASCII, pPercent, pVerbose)
+    print_results(lByteCounts, lTotalBytes, pShowHistogram, pShowASCII, pPercent, pVerbose)
 
 
 if __name__ == '__main__':
@@ -81,6 +88,7 @@ if __name__ == '__main__':
     lArgParser.add_argument('-g', '--show-histogram', help='Show histogram for each byte of input', action='store_true')
     lArgParser.add_argument('-a', '--show-ascii', help='Show ascii representation for each byte of input', action='store_true')
     lArgParser.add_argument('-all', '--show-all', help='Show count, ascii, percent represenation and histogram for each byte of input. Equivalent to -gs', action='store_true')
+    lArgParser.add_argument('-ioc', '--show-ioc', help='Show kappa (delta) index of coincidence', action='store_true')
     lArgParser.add_argument('-v', '--verbose', help='Enables verbose output', action='store_true')
     lArgParser.add_argument('-if', '--input-format', help='Input format can be character, binary, or base64', choices=['character', 'binary', 'base64'], default='character', action='store')
     lArgParser.add_argument('-i', '--input-file', help='Read INPUT from an input file', action='store')
@@ -106,4 +114,7 @@ if __name__ == '__main__':
     if lArgs.show_all:
         lArgs.show_percent = lArgs.show_histogram = lArgs.show_ascii = True
 
-    analyze(lInput, lArgs.show_histogram, lArgs.show_ascii,  lArgs.show_percent, lArgs.verbose)
+    #analyze(lInput, lArgs.show_histogram, lArgs.show_ascii,  lArgs.show_percent, lArgs.verbose)
+
+    if lArgs.show_ioc:
+        print_delta_index_of_coincidence(lInput)

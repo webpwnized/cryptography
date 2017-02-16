ed24e7c28fc4233afa59c7581780be64ef101eb5
webpwnized <webpwnized@gmail.com>
2017-02-16 18:39:50 -0500

    Added entropy calculator

--- freek.py
+++ freek.py
@@ -1,9 +1,33 @@
-import argparse, base64
-from collections import OrderedDict
+import argparse, base64, math
 
 
 TWO_DECIMAL_PLACES = "{0:.2f}"
 MODULUS = 256
+
+
+def get_byte_counts(pInput: bytearray) -> tuple:
+
+    lByteCounts = dict.fromkeys(range(0,256), 0)
+    lTotalBytes = 0
+
+    for lByte in pInput:
+        lByteCounts[lByte] += 1
+        lTotalBytes += 1
+    # end for
+
+    return lByteCounts, lTotalBytes
+
+
+def get_entropy(pByteCounts: dict, pTotalBytes: int) -> float:
+    # Shannon's Entropy -SUM FROM 0 to N-1(Pi * LogBase2(Pi)) where
+    # Pi is the probability of element i
+    BASE2 = 2
+    lEntropy = 0
+    for lByte, lByteCount in pByteCounts.items():
+        lProbabilityOfByte = lByteCount / pTotalBytes
+        if lProbabilityOfByte > 0:
+            lEntropy += -1 * lProbabilityOfByte * math.log(lProbabilityOfByte, BASE2)
+    return lEntropy
 
 
 def get_delta_index_of_coincidence(pInput: bytearray) -> dict:
@@ -73,13 +97,9 @@ def print_byte_analysis(pByte: int, pByteCount: int, pTotalBytes: int, pShowCoun
 
 def print_analysis(pInput: bytearray, pShowCount: bool, pShowHistogram: bool, pShowASCII: bool, pShowPercent: bool, pShowGuesses: bool, pVerbose: bool, pTopFrequencies: int, pColumnarAnalysis: int) -> None:
 
-    lByteCounts = dict.fromkeys(range(0,256), 0)
-    lTotalBytes = 0
+    lByteCounts, lTotalBytes = get_byte_counts(pInput)
 
-    for lByte in pInput:
-        lByteCounts[lByte] += 1
-        lTotalBytes += 1
-    # end for
+    print("Analysis of Input")
 
     if pTopFrequencies:
         lBytesPrinted = 0
@@ -87,6 +107,7 @@ def print_analysis(pInput: bytearray, pShowCount: bool, pShowHistogram: bool, pS
         for lByte, lByteCount in sorted(lByteCounts.items(), key=lambda x:x[1], reverse=True):
             if pVerbose or lByteCount:
                 if pShowGuesses and lAnalyzingMostPopularByte:
+                    # For JPEG we assume the mode of the plaintext is 0, so we guess the mode of the cipher text is offset by X bytes
                     print('\nBest guess\tLowercase: ' + chr((lByte + 97) % MODULUS) + '\tUppercase: ' + chr((lByte + 65) % MODULUS) + '\tNumeric: ' + chr((lByte + 48) % MODULUS))
                     lAnalyzingMostPopularByte = False
                 print_byte_analysis(lByte, lByteCount, lTotalBytes, pShowCount, pShowHistogram, pShowASCII, pShowPercent, pShowGuesses, pVerbose)
@@ -100,6 +121,29 @@ def print_analysis(pInput: bytearray, pShowCount: bool, pShowHistogram: bool, pS
                 print_byte_analysis(lByte, lByteCount, lTotalBytes, pShowCount, pShowHistogram, pShowASCII, pShowPercent, pShowGuesses, pVerbose)
             # end if
     # end for
+    print()
+
+
+def print_columnar_analysis(pInput: bytearray, pShowCount: bool, pShowHistogram: bool, pShowASCII: bool, pShowPercent: bool, pShowGuesses: bool, pVerbose: bool, pTopFrequencies: int, pColumnarAnalysis: int) -> None:
+
+    lColumns = {lColumn: bytearray() for lColumn in range(0, pColumnarAnalysis)}
+    lColumn = 0
+    for lByte in lInput:
+        lColumns[lColumn].append(lByte)
+        lColumn = (lColumn + 1) % lArgs.columnar_analysis
+
+    for lColumn in lColumns:
+        print("Analysis of Column {}".format(lColumn + 1))
+        print_analysis(lColumns[lColumn], lArgs.show_count, lArgs.show_histogram, lArgs.show_ascii, lArgs.show_percent,
+                       lArgs.show_guesses, lArgs.verbose, lArgs.top_frequencies, lArgs.columnar_analysis)
+        print()
+
+
+def print_entropy(pInput: bytearray) -> None:
+    lByteCounts, lTotalBytes = get_byte_counts(lInput)
+    lEntropy = get_entropy(lByteCounts, lTotalBytes)
+    print("Entropy: {}".format(TWO_DECIMAL_PLACES.format(lEntropy)))
+    print()
 
 
 if __name__ == '__main__':
@@ -110,8 +154,9 @@ if __name__ == '__main__':
     lOutputOptions.add_argument('-p', '--show-percent', help='Show percent representation for each byte of input', action='store_true')
     lOutputOptions.add_argument('-m', '--show-histogram', help='Show histogram for each byte of input', action='store_true')
     lOutputOptions.add_argument('-a', '--show-ascii', help='Show ascii representation for each byte of input', action='store_true')
+    lOutputOptions.add_argument('-e', '--show-entropy', help='Show Shannon entropy', action='store_true')
     lOutputOptions.add_argument('-ioc', '--show-ioc', help='Show kappa (delta) index of coincidence', action='store_true')
-    lOutputOptions.add_argument('-all', '--show-all', help='Show count, ascii, percent represenation, histogram for each byte of input. Does NOT include index of coincidence. Equivalent to -cpmag.', action='store_true')
+    lOutputOptions.add_argument('-all', '--show-all', help='Show count, ascii, percent represenation, histogram for each byte of input and Shannon entropy for input. Does NOT include index of coincidence. Equivalent to -cpma.', action='store_true')
     lArgParser.add_argument('-t', '--top-frequencies', help='Only display top X frequencies. Particuarly useful when combined with columnar analysis or when less important bytes clutter analysis.', action='store', type=int)
     lArgParser.add_argument('-g', '--show-guesses', help='Show ascii representation for top byte of input. Tries ASCII lower, upper and numeric translations. Only works with -t/--top-frequencies.', action='store_true')
     lArgParser.add_argument('-col', '--columnar-analysis', help='Break INPUT into X columns and perform analysis on columns. Particuarly useful against polyalphabetic stream ciphers.', action='store', type=int)
@@ -122,7 +167,7 @@ if __name__ == '__main__':
     lInputSource.add_argument('INPUT', nargs='?', help='INPUT to analyze', action='store')
     lArgs = lArgParser.parse_args()
 
-    if lArgs.show_all is False and lArgs.show_percent is False and lArgs.show_histogram is False and lArgs.show_ascii is False and lArgs.show_count is False and lArgs.show_ioc is False:
+    if lArgs.show_all is False and lArgs.show_percent is False and lArgs.show_histogram is False and lArgs.show_ascii is False and lArgs.show_count is False and lArgs.show_ioc is False and lArgs.show_entropy is False:
         lArgParser.error('No output chosen to display. Please choose at least one output option.')
 
     if lArgs.input_file:
@@ -142,24 +187,17 @@ if __name__ == '__main__':
     # end if
 
     if lArgs.show_all:
-        lArgs.show_percent = lArgs.show_histogram = lArgs.show_ascii = lArgs.show_count = True
+        lArgs.show_percent = lArgs.show_histogram = lArgs.show_ascii = lArgs.show_count = lArgs.show_entropy = True
 
     if lArgs.show_percent or lArgs.show_histogram or lArgs.show_ascii or lArgs.show_count:
 
         if lArgs.columnar_analysis:
-            lColumns = {lColumn: bytearray() for lColumn in range(0, lArgs.columnar_analysis)}
-            lColumn = 0
-            lCounter = 0
-            for lByte in lInput:
-                lColumns[lColumn].append(lByte)
-                lColumn = (lColumn + 1) % lArgs.columnar_analysis
-
-            for lColumn in lColumns:
-                print("Analysis of Column {}".format(lColumn+1))
-                print_analysis(lColumns[lColumn], lArgs.show_count, lArgs.show_histogram, lArgs.show_ascii, lArgs.show_percent, lArgs.show_guesses, lArgs.verbose, lArgs.top_frequencies, lArgs.columnar_analysis)
-                print()
+            print_columnar_analysis(lInput, lArgs.show_count, lArgs.show_histogram, lArgs.show_ascii, lArgs.show_percent, lArgs.show_guesses, lArgs.verbose, lArgs.top_frequencies, lArgs.columnar_analysis)
         else:
             print_analysis(lInput, lArgs.show_count, lArgs.show_histogram, lArgs.show_ascii, lArgs.show_percent, lArgs.show_guesses, lArgs.verbose, lArgs.top_frequencies, lArgs.columnar_analysis)
 
     if lArgs.show_ioc:
         print_delta_index_of_coincidence(lInput)
+
+    if lArgs.show_entropy:
+        print_entropy(lInput)
